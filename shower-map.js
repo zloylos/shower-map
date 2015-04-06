6831e152702fb1d9b35a293da01a2346a716cd75
Denis Hananein <i@zloylos.me>
2015-04-06 19:27:48 +0300

    release: doc, fix bugs

--- shower-map.js
+++ shower-map.js
@@ -1,14 +1,19 @@
 modules.define('shower-map', [
     'shower',
+    'util.bind',
     'util.extend'
-], function (provide, shower, extend) {
+], function (provide, shower, bind, extend) {
 
-    var MAP_ELEMENT = 'ymap',
-        PLACEMARK_ELEMENT = 'placemark',
+    var MAP_ELEMENT_SELECTOR = 'ymap',
+        PLACEMARK_ELEMENT_SELECTOR = 'placemark',
         BASE_API_PATH = 'https://api-maps.yandex.ru/',
         DEFAULT_MAP_OPTIONS = {
-            center: [54, 32],
-            zoom: 12
+            mapType: 'yandex#map',
+            center: [55.755768, 37.617671],
+            zoom: 15,
+            controls: 'smallMapDefaultSet',
+            width: '100%',
+            height: '80%'
         },
         DEFAULT_API_OPTIONS = {
             version: '2.1',
@@ -25,6 +30,10 @@ modules.define('shower-map', [
      * @param {object} [options]
      * @param {number[]} [options.center = [54, 32]]
      * @param {number} [options.zoom = 12] 
+     * @param {string} [options.mapType = 'yandex#map']
+     * @param {string} [options.controls = 'smallMapDefaultSet'] 
+     * @param {string|number} [options.width = '100%']
+     * @param {string|number} [options.height = '80%']
      * @param {object} [options.api]
      * @param {string} [options.api.version = '2.1']
      * @param {string} [options.api.mode = 'release']
@@ -50,7 +59,7 @@ modules.define('shower-map', [
         options.api = options.api || {};
 
         this._shower = shower;
-
+        
         this.options = extend({}, DEFAULT_MAP_OPTIONS, options);
         this.options.api = extend({}, DEFAULT_API_OPTIONS, options.api); 
 
@@ -59,23 +68,43 @@ modules.define('shower-map', [
 
     extend(ShowerMap.prototype, /** @lends ShowerMap.prototype */ {
         init: function () {
-            this._initApi();
-            ymaps.ready().then(this._initMaps, this);
+            var isApiDefined = this._isApiDefined();
+
+            // If JS API already loaded — 
+            if (isApiDefined) {
+                ymaps.ready().then(this._initMaps, this);
+            } else {
+                this._initApi();
+
+                // Wait JS API loader.
+                var checkInterval = setInterval(bind(function () {
+                    if (this._isApiDefined()) {
+                        ymaps.ready().then(this._initMaps, this);
+                        clearInterval(checkInterval);
+                    }
+                }, this), 15);
+            }
         },
 
         /**
-         * @returns {ymaps.Map[]}
+         * @returns {ymaps.Map[]} Array of inited maps.
          */
         getMaps: function () {
             return this._maps;
         },  
 
+        /**
+         * @ignore
+         * Init JS API Yandex.Maps.
+         */
         _initApi: function () {
             var apiOptions = this.options.api,
-                apiScriptPath = BASE_API_PATH + 
-                    apiOptions.version + '/' +
-                    '?lang=' + apiOptions.lang, 
-                    '&mode=' + apiOptions.mode,
+                apiScriptPath = [
+                    BASE_API_PATH,
+                    apiOptions.version + '/',
+                    '?lang=' + apiOptions.lang,
+                    '&mode=' + apiOptions.mode
+                ].join(''),
                 apiScriptElement = document.createElement('script');
 
             document.body.appendChild(apiScriptElement);
@@ -83,17 +112,23 @@ modules.define('shower-map', [
         },
 
         _initMaps: function () {
-            var elements = this._findMapElements();
+            var elements = this._find(MAP_ELEMENT_SELECTOR);
             elements.forEach(this._initMap, this);
         },
 
         /**
+         * @ignore
          * @param {HTMLElement} mapElement
          */
         _initMap: function (mapElement) {
             var data = mapElement.dataset,
                 center = data.center,
                 zoom = data.zoom, 
+                controls = data.controls || this.options.controls,
+                width = data.width || this.options.width,
+                height = data.height || this.options.height,
+                mapType = data.maptype || this.options.mapType,
+                mapElementStyle = mapElement.style,
                 map;
 
             if (center) {
@@ -104,69 +139,89 @@ modules.define('shower-map', [
                 zoom = parseInt(zoom);
             }
 
-            map = this._maps.push(new ymaps.Map(mapElement, {
-                zoom: zoom || this.options.zoom,
-                center: center || this.options.center
-            }));
+            if (typeof width == 'number') {
+                width += 'px';
+            }
 
-            this._initPlacemarks();
+            if (typeof height == 'number') {
+                height += 'px';
+            }
+
+            mapElementStyle.display = 'block';
+            mapElementStyle.width = width;
+            mapElementStyle.height = height;
+
+            map = new ymaps.Map(mapElement, {
+                type: mapType,
+                zoom: zoom || this.options.zoom,
+                center: center || this.options.center,
+                controls: controls.split(',')
+            });
+
+            this._maps.push(map);
+            this._initPlacemarks(map, mapElement);
         },
 
         /**
+         * @ignore
          * @param {ymaps.Map} map
          * @param {HTMLElement} container
          */
         _initPlacemarks: function (map, container) {
-            var elements = this._findPlacemarkElements();
-            elements.forEach(function () {
-                this._initPlacemark(map, elem);
+            var elements = this._find(PLACEMARK_ELEMENT_SELECTOR, container);
+            elements.forEach(function (element) {
+                this._initPlacemark(map, element);
             }, this);
         },
 
         /**
+         * @ignore
          * @param {ymaps.Map} map
-         * @param {HTMLElement} elem
+         * @param {HTMLElement} element
          */
-        _initPlacemark: function (map, elem) {
-            var data = elem.dataset,
+        _initPlacemark: function (map, element) {
+            var data = element.dataset,
                 coords = data.coords,
                 balloonContent = data.balloon
                 hintContent = data.hint;
 
             coords = parseCoord(coords);
 
-            map.geoObject.add(new ymaps.Placemark(coords, {
+            map.geoObjects.add(new ymaps.Placemark(coords, {
                 balloonContent: balloonContent,
                 hintContent: hintContent
             }));
         },
 
-        _findMapElements: function () {
-            var showerContainerElement = shower.container.getElement(),
-                mapElements = showerContainerElement.querySelectorAll(MAP_ELEMENT);
-
-            return Array.prototype.slice.call(mapElements);
+        _isApiDefined: function () {
+            return typeof ymaps !== 'undefined';
         },
 
-        _findPlacemarkElements: function () {
-            var elements = container.querySelectorAll(PLACEMARK_ELEMENT);
-            return Array.prototype.slice(elements);
+        _find: function (selector, container) {
+            container = container || this._shower.container.getElement();
+            var elements = container.querySelectorAll(selector);
+            return Array.prototype.slice.call(elements);
         }
-    };
+    });
 
     /**
-     * @param {string}
-     * @return {number[]}
+     * @ignore
+     * Parse coordinates.
+     * @param {string} coords
+     * @return {number[]} Parsed coords.
      */
-    function parseCoord (coord) {
-        coord = coord.split(',');
-        coord.forEach(function (coord) {
+    function parseCoord (coords) {
+        coords = coords.split(',');
+        coords = coords.map(function (coord) {
             return parseFloat(coord.trim());
         });
 
-        return coord;
+        return coords;
     }
 
     provide(ShowerMap);
+});
+
+modules.require(['shower'], function (shower) {
     shower.plugins.add('shower-map');
 });
